b068b2a26608b51f3602bbe946193595e443944c
VisarMarku00 <96630229+VisarMarku00@users.noreply.github.com>
2025-10-08 19:38:20 +0200

    Refactored Script.js file

--- script.js
+++ script.js
@@ -1,213 +1,185 @@
-let board = [
-  ["0", "1", "2"],
-  ["3", "4", "5"],
-  ["6", "7", "8"],
-];
-
-const gridOfCells = document.getElementById("board");
-const cells = document.querySelectorAll(".cell");
-let currentPlayer = "X";
-let playerTurn = document.getElementById("player-turn");
-let countTurns = 0;
-let playerChange = true;
-
-let row;
-let column;
-
-let scoreX = 0;
-let scoreO = 0;
-let scoreXHtml = document.getElementById("score-x");
-let scoreOHtml = document.getElementById("score-o");
-
-let playAgain = document.getElementById("play-again");
-
-function addSymbol(cell) {
-  if (cell.innerText !== "") return;
-  cell.innerText = currentPlayer;
-  const placeInBoard = cell.id;
-  cell.style.pointerEvents = "none";
-
-  row = placeInBoard.charAt(0);
-  column = placeInBoard.charAt(1);
-
-  board[row][column] = currentPlayer;
-}
-
-function changePlayer(gameEnd, playerChange) {
-  if (gameEnd) return;
-  if (!playerChange) return;
-  if (currentPlayer === "X") {
-    currentPlayer = "O";
-  } else {
-    currentPlayer = "X";
-  }
-
-  playerTurn.textContent = `${currentPlayer}'s turn`;
-  countTurns++;
-}
-
-function nextRound() {
-  if (currentPlayer === "X") {
-    currentPlayer = "O";
-  } else {
-    currentPlayer = "X";
-  }
-
-  playerTurn.textContent = `${currentPlayer}'s turn`;
-}
-
-function checkScore(countTurns) {
-  if (countTurns > 3) {
-    for (let i = 0; i <= 2; i++) {
-      {
-        if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
-          return true;
-        } else if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
-          return true;
-        }
-      }
-    }
-    if (
-      (board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
-      (board[2][0] === board[1][1] && board[2][0] === board[0][2])
-    ) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function checkWinner(gameEnded) {
-  if (gameEnded) {
-    playerTurn.textContent = `${currentPlayer} wins`;
-    countTurns = 0;
-    playAgain.style.visibility = "visible";
-    gridOfCells.style.pointerEvents = "none";
-    if (currentPlayer == "X") {
-      scoreX++;
-      scoreXHtml.innerText = scoreX;
-    } else {
-      scoreO++;
-      scoreOHtml.innerText = scoreO;
-    }
-  } else if (countTurns === 8) {
-    playerTurn.textContent = `Tie game!`;
-    countTurns = 0;
-    playAgain.style.visibility = "visible";
-    playerChange = false;
-  }
-}
-
-cells.forEach((cell) => {
-  cell.addEventListener("click", () => {
-    addSymbol(cell);
-    const gameEnd = checkScore(countTurns);
-    checkWinner(gameEnd);
-    changePlayer(gameEnd, playerChange);
-  });
-});
-
-playAgain.addEventListener("click", () => {
-  cells.forEach((cell) => {
-    cell.innerText = "";
-    cell.style.pointerEvents = "";
-  });
-  board = [
-    ["0", "1", "2"],
-    ["3", "4", "5"],
-    ["6", "7", "8"],
-  ];
-  nextRound();
-  countTurns = 0;
-  playAgain.style.visibility = "hidden";
-  gridOfCells.style.pointerEvents = "";
-  playerChange = !playerChange ? true : playerChange;
-});
-
-//////////////////////////////////////////////////////////////////////////////////
-
-function createPlayer(name) {
-  const playerName = name;
-
-  let score = 0;
-  const addToScore = () => score++;
-
-  return { playerName, addToScore };
-}
-
-function GameLogic() {
+//Game logic
+const GameLogic = () => {
   let board = [
-    ["0", "1", "2"],
-    ["3", "4", "5"],
-    ["6", "7", "8"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
-
-  let countTurns = 0;
-  let playerChange = true;
-
-  let row;
-  let column;
-
-  let scoreX = 0;
-  let scoreO = 0;
-
-  function placeInboard() {}
-
-  function changePlayer() {}
-
-  function nextRound() {}
-
-  function checkScore() {}
-
-  function checkWinner() {}
-
-  function playAgain() {}
-
-  return {
-    placeInboard,
-    changePlayer,
-    nextRound,
-    checkScore,
-    checkWinner,
-    playAgain,
-  };
-}
-
-function UiManipulation() {
-  const gridOfCells = document.getElementById("board");
-  const cells = document.querySelectorAll(".cell");
 
   let currentPlayer = "X";
-  let playerTurn = document.getElementById("player-turn");
+  let countTurns = 0;
+  let playerChange = true;
+  let row;
+  let column;
+  let scoreX = 0;
+  let scoreO = 0;
+  let winner = "";
 
-  let scoreXHtml = document.getElementById("score-x");
-  let scoreOHtml = document.getElementById("score-o");
+  function placeInBoard(row, column) {
+    if (board[row][column] === "") {
+      board[row][column] = currentPlayer;
+    }
 
-  let playAgain = document.getElementById("play-again");
+    if (countTurns >= 4) {
+      checkBoard();
+    }
 
-  function addSymbol() {}
+    if (winner !== "") {
+      return;
+    }
 
-  function playerTurnSignifier() {}
+    changePlayer();
 
-  function nextRoundSignifier() {}
+    countTurns++;
+  }
 
-  function modifyUiAccoringToWinner() {}
+  function changePlayer() {
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
+  }
 
-  function makeCellsFunctional() {}
+  function checkBoard() {
+    if (countTurns >= 4) {
+      for (let i = 0; i <= 2; i++) {
+        {
+          if (board[i][0] !== "" && board[i][1] !== "" && board[i][2] !== "") {
+            if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+              winner = currentPlayer;
+            }
+          }
+          if (board[0][i] !== "" && board[1][i] !== "" && board[2][i] !== "") {
+            if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+              winner = currentPlayer;
+            }
+          }
+        }
+      }
+      if (
+        (board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+        (board[2][0] === board[1][1] && board[2][0] === board[0][2])
+      ) {
+        winner = currentPlayer;
+      }
+    }
+  }
 
-  function resetUi() {}
+  function winnerScore() {
+    if (winner === "X") {
+      return ++scoreX;
+    } else if (winner === "O") {
+      return ++scoreO;
+    }
+  }
+
+  function resetGame() {
+    board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    countTurns = 0;
+    currentPlayer = "X";
+    winner = "";
+  }
 
   return {
-    addSymbol,
-    changePlayer,
-    playerTurnSignifier,
-    nextRoundSignifier,
-    modifyUiAccoringToWinner,
-    makeCellsFunctional,
-    resetUi,
+    getBoard() {
+      return board;
+    },
+    placeInBoard,
+    getPlayer() {
+      return currentPlayer;
+    },
+    winnerScore,
+    getWinner() {
+      return winner;
+    },
+    resetGame,
+    getCountTurns(){
+      return countTurns;
+    }
   };
-}
+};
 
-let playerX = createPlayer("X");
+//UI logic
 
-let playerO = createPlayer("O");
+const UiLogic = () => {
+  const game = GameLogic();
+  const gridOfCells = document.getElementById("board");
+  const cells = document.querySelectorAll(".cell");
+  let playerTurn = document.getElementById("player-turn");
+  let scoreXHtml = document.getElementById("score-x");
+  let scoreOHtml = document.getElementById("score-o");
+  let playAgainButton = document.getElementById("play-again");
+
+  function renderBoard() {
+    let board = game.getBoard();
+    cells.forEach((cell) => {
+      let placeInArray = cell.id;
+      let row = placeInArray.charAt(0);
+      let column = placeInArray.charAt(1);
+
+      cell.textContent = board[row][column];
+    });
+  }
+
+  function makeCellsFunctional() {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        let placeInArray = cell.id;
+        let row = placeInArray.charAt(0);
+        let column = placeInArray.charAt(1);
+
+        game.placeInBoard(row, column);
+        renderBoard();
+        if (game.getWinner() != "" || game.getCountTurns() === 9) {
+          endGameScreen();
+        } else {
+          playerTurn.textContent = `${game.getPlayer()}'s turn`;
+        }
+      });
+    });
+  }
+
+  function playAgain() {
+      game.resetGame();
+      playAgainButton.style.visibility = "hidden";
+      gridOfCells.style.pointerEvents = "";
+      playerTurn.textContent = `${game.getPlayer()}'s turn`;
+      renderBoard();
+  }
+
+  function endGameScreen() {
+    let winner = game.getWinner();
+    if (winner == "X" || winner == "O") {
+      playerTurn.textContent = `${winner} wins`;
+      if (winner == "X") {
+        scoreXHtml.innerText = game.winnerScore();
+      } else {
+        scoreOHtml.innerText = game.winnerScore();
+      }
+    } else {
+      playerTurn.textContent = `Tie game!`;
+    }
+
+    gridOfCells.style.pointerEvents = "none";
+    playAgainButton.style.visibility = "visible";
+  }
+
+  function init() {
+    renderBoard();
+    makeCellsFunctional();
+    playAgainButton.addEventListener("click", playAgain);
+    playAgainButton.style.visibility = "hidden";
+    playerTurn.textContent = `${game.getPlayer()}'s turn`;
+  }
+
+  return {
+    init,
+  };
+};
+
+UiLogic().init();
